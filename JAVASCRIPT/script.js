2065a58a7ddafde5436b7094b73b45f17d76b8d3
sapitri12345 <sapitripipit04@gmail.com>
2023-09-26 11:12:24 +0700

    Belajar Javascript 1

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,21 @@
+document.write("Eksternal Javascript")
+prompt("apakah belajar javascript menyenangkan")
+
+let nama = "sapitri";
+console.log(nama);
+
+let usia;
+usia = 12;
+console.log(usia);
+
+
+let nama1 = "sapitri", age = 13, alamat = "bogor";
+console.log(nama1, age, alamat);
+
+let x = 10;
+x = 15;
+console.log(x);
+
+const x1 = 10;
+x1 = 15;
+console.log(x1);
