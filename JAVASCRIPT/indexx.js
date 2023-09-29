e203b135cdc1e01b683cec1c11b51dab4138c61b
sapitri12345 <sapitripipit04@gmail.com>
2023-09-30 00:13:17 +0700

    Web Portofolio

---
+++ JAVASCRIPT/indexx.js
@@ -0,0 +1,14 @@
+const hewan = [
+    'kucing',
+    'ayam',
+    'bebek'
+]
+
+const hewan2 = []
+    hewan2[0] = 'kucing',
+    hewan2[1] = 'ayam',
+    hewan2[2] = 'bebek'
+
+    const hewan3 = new Array('kucing', 'ayam', 'bebek')
+
+    console.log
